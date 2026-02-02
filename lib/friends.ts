ad8e3d0b34c4daf15318d45bca3248b01d6308e5
Michael Vergotis <michaelvergotis@gmail.com>
2026-02-02 20:06:53 +1000

    Add Nick Condo to friends list

--- lib/friends.ts
+++ lib/friends.ts
@@ -9,6 +9,7 @@ export const friends: Friend[] = [
   { id: "sam-d", name: "Sam D", initials: "SD" },
   { id: "julian-z", name: "Julian Z", initials: "JZ" },
   { id: "michael-v", name: "Michael V", initials: "MV" },
+  { id: "nick-c", name: "Nick Condo", initials: "NC" },
 ];
 
 export function getFriendById(id: string): Friend | undefined {
